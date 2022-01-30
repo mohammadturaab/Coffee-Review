303768c39641c830fb9005c643db20f95dcbae41
Mohammad Turaab <mohammadturaab@gmail.com>
2022-01-30 16:06:36 -0600

    created user schema and model

--- model/index.js
+++ model/index.js
@@ -1,7 +1,17 @@
 const mongoose =  require('mongoose');
 const Schema = mongoose = new Schema;
 
+
+
 const userSchema = new Schema ({
     name: { type: String, require: true},
-
+    email: String,
+    avatarURL: String,
+    review: [reviewSchema],
+    googleId: String, 
+},
+{
+    timestamps: true
 });
+
+module.exports = mongoose.model('User', userSchema);
