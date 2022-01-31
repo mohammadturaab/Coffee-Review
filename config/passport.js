bfc462acef58943124be95631ce4b45d8a8f45d1
Mohammad Turaab <mohammadturaab@gmail.com>
2022-01-30 21:12:03 -0600

    Added find a user and create new user using google auth

--- config/passport.js
+++ config/passport.js
@@ -2,23 +2,40 @@ const passport = require('passport');
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-passport.serializeUser(function(user, done) {
-        done(null, user);
-    });
-    
-passport.deserializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
         done(null, user);
 });
-    
+
+passport.deserializeUser(function (user, done) {
+        done(null, user);
+});
+
 
 
 passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK,
-        passReqToCallback   : true
-    },
-    function(request, accessToken, refreshToken, profile, done) {
-            return done(null, profile);
-    }
+                clientID: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_SECRET,
+                callbackURL: process.env.GOOGLE_CALLBACK,
+                passReqToCallback: true
+        },
+        function (request, accessToken, refreshToken, profile, done) {
+                User.findOne({
+                        googleId: profile.id
+                }, function (err, user) {
+                        if (err) return cb(err);
+                        if (user) {
+                                return (null, user);
+                        } else {
+                                const newUser = new User({
+                                        name: profile.displayName,
+                                        email: profile.emails[0].value,
+                                        googleId: profile.id,
+                                });
+                                newUser.sace(function (err) {
+                                        if (err) return cb(err);
+                                        return cb(null, newUser);
+                                })
+                        }
+                })
+        }
 ));
