d85fe48212cf61b83fda71e7c47873b35ec864c0
Moises-Giron <moisesgiron2@gmail.com>
2022-02-01 10:38:40 -0600

    Fixes the serving of static files and renders the index page on root

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 
-require('./config/database');
+// require('./config/database');
 require('./config/passport');
 
 
 const app = express();
 
 const session = require("express-session");
-const req = require('express/lib/request');
 const passport = require('passport');
 
 const routes = require('./routes/');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
+
 
 app.use(
     session({
@@ -28,13 +28,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', routes.googleAuth);
+app.set('view engine', 'ejs');
 
+
+app.use('/', routes.googleAuth);
 app.get('/', (req, res) => {
-    res.send('You are not logged in')
+    res.render('index');
 });
 
-app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
