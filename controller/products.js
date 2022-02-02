69d7ff6681c8d0b89a973f5c9cd9913ab9afa47d
Mohammad Turaab <mohammadturaab@gmail.com>
2022-02-01 19:41:20 -0600

    ejs created for products

--- controller/products.js
+++ controller/products.js
@@ -1,12 +1,9 @@
 const Product = require('../model/products');
 
-
 function index(req, res){
     Product.find({}, function(err, product){
         if (err) res.send(err);
 
-        const context = { allProducts: product}
-        
         res.render('products/index');
     })
 }
