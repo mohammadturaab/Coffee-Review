da400a3d58cb3717d5b2f7750e2f9d3558f9e4a9
Mohammad Turaab <mohammadturaab@gmail.com>
2022-02-01 19:03:50 -0600

    added coffee inside the database

--- controller/products.js
+++ controller/products.js
@@ -5,7 +5,8 @@ function index(req, res){
     Product.find({}, function(err, product){
         if (err) res.send(err);
 
-        const context = { product: product}
+        const context = { allProducts: product}
+        
         res.render('products/index');
     })
 }
