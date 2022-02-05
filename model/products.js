385338596725a23674657c334d97956680eda0f2
Moises-Giron <moisesgiron2@gmail.com>
2022-02-05 14:05:51 -0600

    References the Review model

--- model/products.js
+++ model/products.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
     name: {type: String, required: true},
     description: {type: String,},
-    image: {String}
+    image: {String},
+    reviews: [{type: Schema.Types.ObjectId, ref:'Review'}]
 })
 
 const Product = mongoose.model('Products', productSchema);
