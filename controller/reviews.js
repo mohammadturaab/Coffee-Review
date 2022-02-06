26d2560c35f2d9de72a195e7946b11b72840923a
Moises-Giron <moisesgiron2@gmail.com>
2022-02-05 18:52:58 -0600

    Modifies single review page to get all reviews for one product

--- controller/reviews.js
+++ controller/reviews.js
@@ -1,20 +1,25 @@
 const Product = require('../model/products');
 const User = require('../model/index');
+const Review = require('../model/reviews');
 
 
 function displaySingleReview (req, res){
-
+    //Search product, then pull in all of its reviews.
     Product.findById(req.params.id, (err, product) => {
         if (err) res.send(err);
-        console.log(product);
-        res.render('products/singleReview',{
-            product,
-            products: req.product,
-            user: req.user
+        Review.find({product: product._id}, (err, reviewFound) => {
+            if(err) res.send(err);
+            res.render('products/singleReview',{
+                product,
+                products: req.product,
+                user: req.user,
+                reviewFound
+            });
         });
     });
 }
 
+
 module.exports = {
-    displaySingleReview
+    displaySingleReview,
 }
