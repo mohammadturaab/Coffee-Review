2bc414571fe970d06c99b137dc82ed398157a310
Mohammad Turaab <mohammadturaab@gmail.com>
2022-02-06 12:57:01 -0600

    added the delete button to the review posts.

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,14 +8,8 @@ router.get('/', ctrl.displayReviewPage);
 
 router.get('/:id', ctrl.displaySingleReview);
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/auth/google");
-  }
+router.post('/:id', ctrl.postReview);
 
-router.post('/:id', isLoggedIn, ctrl.postReview);
-
-router.delete("/:id", isLoggedIn, ctrl.destroy);
-
+router.delete("/:id", ctrl.destroy);
 
 module.exports = router;
