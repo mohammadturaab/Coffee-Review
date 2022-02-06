ada7bcb5669953618b675031dadbbfeecb902ab6
Moises-Giron <moisesgiron2@gmail.com>
2022-02-05 18:50:01 -0600

    Provides routing for posting individual reviews and getting all in the review page

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const ctrl = require('../controller/reviews');
 
-router.get('/', (req, res) => {
-    res.render('products/reviews');
-});
+router.get('/', ctrl.displayReviewPage);
 
 router.get('/:id', ctrl.displaySingleReview);
+
+router.post('/:id', ctrl.postReview);
 
 
 module.exports = router;
