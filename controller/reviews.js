07a4df592290d69f60263b77e82a0026c1b3f5c2
Moises-Giron <moisesgiron2@gmail.com>
2022-02-05 18:54:28 -0600

    Provides function for displaying all products and reviews on main review page

--- controller/reviews.js
+++ controller/reviews.js
@@ -2,6 +2,22 @@ const Product = require('../model/products');
 const User = require('../model/index');
 const Review = require('../model/reviews');
 
+function displayReviewPage(req, res){
+    //Find all products and reviews
+    Product.find({}, (err, product) => {
+        if(err) res.send(err);
+        Review.find({}, (err, reviewFound) => {
+            console.log(reviewFound);
+            if(err) res.send(err);
+            res.render('products/reviews', {
+                product,
+                products: req.product,
+                user: req.user,
+                reviewFound
+            });     
+        })
+    });
+}
 
 function displaySingleReview (req, res){
     //Search product, then pull in all of its reviews.
@@ -21,5 +37,6 @@ function displaySingleReview (req, res){
 
 
 module.exports = {
+    displayReviewPage,
     displaySingleReview,
 }
