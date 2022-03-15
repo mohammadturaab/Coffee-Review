edf8fd3d5bdfdac3f9b5ed680832f9422d35d3b8
moises-giron-01 <moisesgiron2@gmail.com>
2022-03-14 21:52:41 -0500

    Removes unneeded comments and duplicate code

--- routes/products.js
+++ routes/products.js
@@ -3,8 +3,5 @@ const productsCtrl = require('../controller/products');
 
 
 router.get('/', (productsCtrl.index));
-// router.get('/', (req, res) => {
-//     res.send("Hello World");
-// });
 
 module.exports = router;
